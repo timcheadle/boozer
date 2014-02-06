42420190cdb0c0885479fed22235fa83073a645c
Tim Cheadle <tim@rationalmeans.com>
2014-02-06 01:14:19 -0500

    Fleshed out basic crawler and CSV output

--- boozer.rb
+++ boozer.rb
@@ -3,17 +3,53 @@
 require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
+require 'csv'
 
 PRODUCT_URL = 'http://www.meckabc.com/Products'
 
-doc = Nokogiri::HTML(open(PRODUCT_URL))
+root_doc = Nokogiri::HTML(open(PRODUCT_URL))
 
-categories = doc.xpath('//select[contains(@id,"AlcoholType")]/option[@value != "-1"]').map { |node| node.attribute('value').value.strip }
+categories = root_doc.xpath('//select[contains(@id,"AlcoholType")]/option[@value != "-1"]').map { |node| node.attribute('value').value.strip }
+
+inventory = {}
 
 categories.sort.each do |category|
   url = URI::encode("http://www.meckabc.com/Products?t=#{category}&d=&c=")
   doc = Nokogiri::HTML(open(url))
 
-  headers = doc.xpath('//table[contains(@id,"SearchResults")]/*/th').map { |node| node.text.strip }
-  rows    = doc.xpath('//table[contains(@id,"SearchResults")]/*/tr[not(descendant::th)]')
+  next unless doc
+
+  results = doc.css('table[id$="SearchResults"]')
+
+  headers = results.css('th').map { |node| node.text.strip }
+  rows    = results.css('tr')
+
+  rows.each do |row|
+    cells = row.css('> td')
+
+    if cells.any?
+      sku, brand, type, description, size, price = cells.map { |node| node.text.strip }
+
+      inventory[sku] = {
+        brand: brand,
+        type: type,
+        description: description,
+        size: size,
+        price: price,
+      }
+    end
+  end
+end
+
+inventory.keys.sort.each do |sku|
+  product = inventory[sku]
+
+  puts [
+    sku,
+    product[:brand],
+    product[:type],
+    product[:description],
+    product[:size],
+    product[:price],
+  ].to_csv
 end
